8d4d708a1a340980b30e2204aa27f185287092ef
Rafael Pilliard-Hellwig <rafael.taph@gmail.com>
2021-01-22 12:48:09 -0800

    remove rate() function

--- src/helper.py
+++ src/helper.py
@@ -3,22 +3,6 @@
 import pandas as pd
 
 
-def rate(x, y):
-    """Safely calculates the rate of x and y
-
-    Calculates the rate of x/y where both x and y are not missing
-
-    Args:
-        x ([Series]): any numeric series
-        y ([Series]): any numeric series
-
-    Returns:
-        [float]: rate
-    """
-    condition = x.isna() | y.isna()
-    return sum(x.loc[~condition]) / sum(y.loc[~condition])
-
-
 def sex_selection(input):
     """Return Sex Input Variable as a List"""
     return ["Male", "Female"] if input == "Both" else [input]
