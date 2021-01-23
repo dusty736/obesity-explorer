b86c1739cb641efd2293b00b80ff8e93ab37e4d2
Rafael Pilliard-Hellwig <rafael.taph@gmail.com>
2021-01-22 23:21:50 -0800

    Add documentation and sources to the dashboard

--- src/app.py
+++ src/app.py
@@ -36,9 +36,24 @@ css_dd = {
     "font-size": "smaller",
 }
 
+css_sources = {
+    "font-size": "xx-small",
+}
+
 app.layout = dbc.Container(
     [
         html.H1("Obesity Dashboard"),
+        html.P(
+            dcc.Markdown(
+                """
+                Obesity has been an increasing medical concern across the world in the 
+                21st century. It is a medical precursor to diseases such as diabetes, 
+                heart diseases, high blood pressure and certain types of cancers.
+                This dashboard allows you to explore trends, probe associations, and 
+                discover other patterns related to this global epidemic.
+                """
+            )
+        ),
         dbc.Row(
             [
                 dbc.Col(
@@ -155,6 +170,27 @@ app.layout = dbc.Container(
                                 for i in range(1975, 2017, 5)
                             },
                         ),
+                        html.Hr(),
+                        html.P(
+                            dcc.Markdown(
+                                """
+                                ##### Data Sources
+
+                                The World Bank (n.d.). Indicators. https://data.worldbank.org/indicator. 
+                                Retrieved January 16, 2021, from https://data.worldbank.org/indicator
+
+                                World Health Organization(WHO) (n.d.). 
+                                Prevalence of obesity among adults, BMI = 30 (age-standardized estimate) (%). 
+                                Retrieved January 16, 2021, from 
+                                https://www.who.int/data/gho/data/indicators/indicator-details/GHO/prevalence-of-obesity-among-adults-bmi-=-30-(age-standardized-estimate)-(-)
+
+
+                                ---
+                                This dashboard is created by [dusty736](https://github.com/dusty736), [tanmaysharma19](https://github.com/tanmaysharma19), [jraza19](https://github.com/jraza19), and [rtaph](https://github.com/rtaph). View the source code and contribute [here](https://github.com/UBC-MDS/obesity-explorer).
+                                """
+                            ),
+                            style=css_sources,
+                        ),
                     ],
                     md=4,
                     style={
@@ -182,6 +218,7 @@ app.layout = dbc.Container(
                 ),
             ],
         ),
+        html.Br(),
     ]
 )
 
